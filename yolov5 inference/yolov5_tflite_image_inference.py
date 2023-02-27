81590291026a5bca913227d20eb061efcfdd46ad
Ritik <shrestharitik@gmail.com>
2023-02-27 08:40:54 +0545

    fixed sorting v1

--- yolov5 inference/yolov5_tflite_image_inference.py
+++ yolov5 inference/yolov5_tflite_image_inference.py
@@ -7,7 +7,7 @@ import numpy as np
 from utils import letterbox_image, scale_coords
 
 
-def sortFunc(a):
+def sortFunc(data):
     store = ""
     # for k, v in value.items():
     #     print(k,v[0][0], v[1])
@@ -19,22 +19,48 @@ def sortFunc(a):
     # for i in sorted_values:
     #     store += i[1]
     # print(store)
-    a.sort(key=lambda y: y[0], reverse=True)    # sort by y
-    a.sort(key=lambda x: x[1])                  # sort by x
-    print(a)
-    first_line = a[:4]
-    first_line.sort(key=lambda y: y[0])
-    second_line = a[4:8]
-    second_line.sort(key=lambda y: y[0])
-    third_line = a[8:]
-    third_line.sort(key=lambda y: y[0])
-    print("first_line", first_line)
-    print("second_line", second_line)
-    print("third_line", third_line)
-    sorted_a = first_line + second_line + third_line                # sort by x
     # print(a)
-    for i in range(len(a)):
-        store += sorted_a[i][3]
+
+    # a.sort(key=lambda y: y[0], reverse=True)    # sort by y
+    # a.sort(key=lambda x: x[1])                  # sort by x
+    # first_line = a[:4]
+    # first_line.sort(key=lambda y: y[0])
+    # second_line = a[4:8]
+    # second_line.sort(key=lambda y: y[0])
+    # third_line = a[8:]
+    # third_line.sort(key=lambda y: y[0])
+    # print("first_line", first_line)
+    # print("second_line", second_line)
+    # print("third_line", third_line)
+    # sorted_a = first_line + second_line + third_line                # sort by x
+    # # print(a)
+    # for i in range(len(a)):
+    #     store += sorted_a[i][3]
+    # print(store)
+    # return store
+    # print("Before", data)
+    data.sort(key=lambda x: x[1])
+    # print("After", data)
+
+    threshold = 30
+    fData = data[0][1]
+    # print("Before fData", fData)
+
+    i = 0
+    for d in data:
+        # print("fData value", fData-data[i][1])
+        if abs(fData-data[i][1]) > threshold:
+            fData = data[i][1]
+            # print("After fData", fData)
+        data[i].append(fData)
+        # print("data", data)
+        i = i+1
+    data.sort(key=lambda x: (x[3], x[0]))
+    # print("next sort", data)
+    for d in data:
+        del(d[3])
+    for i in data:
+        store += i[2]
     print(store)
     return store
 
@@ -98,7 +124,9 @@ def detect_image(weights, labels, image_url, img_size, conf_thres, iou_thres):
                 # result.append(
                 #     {round(r[0], 2): result_class_names[i]})
                 result.append(
-                    [round(r[0], 2), round(r[1], 2), int(100*result_scores[i]), result_class_names[i]])
+                    [round(r[0], 2), round(r[1], 2), result_class_names[i]])
+                # result.append(
+                #     [round(r[0], 2), round(r[1], 2), int(100*result_scores[i]), result_class_names[i]])
 
                 # cv2.imshow("frame2", labelImg[:, :, ::-1])
                 print("detect character")
